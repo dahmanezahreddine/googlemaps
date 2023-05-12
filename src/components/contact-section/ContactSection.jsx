ae0f8f63554b0b99c5165acd04136489beedd33e
zahreddine <dahmanezahreddine@gmail.com>
2023-05-12 11:59:50 +0100

    first commit

--- src/components/contact-section/ContactSection.jsx
+++ src/components/contact-section/ContactSection.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 
-import Form from './Form'
-import Info from './Info'
+import Form from "./Form";
+import Info from "./Info";
+import Maps from "../Maps";
 
-import './contact-section.css'
+import "./contact-section.css";
 
+const location = {
+  address: "1600 Amphitheatre Parkway, Mountain View, california.",
+  lat: 37.42216,
+  lng: -122.08427,
+};
 const ContactSection = () => (
   <>
     <div className="contact-section">
       <Form />
       <Info />
+      <Maps />
     </div>
   </>
-)
+);
 
-export default ContactSection
+export default ContactSection;
